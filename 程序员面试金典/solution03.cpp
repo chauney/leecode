00b88e86074cbf03ba838f7c105969226d491493
chauneyWang <13057530802@163.com>
2024-06-30 16:52:10 +0800

    Update solution03.cpp

--- 程序员面试金典/solution03.cpp
+++ 程序员面试金典/solution03.cpp
@@ -23,4 +23,27 @@ public:
 
         return res.size() <= 32 ? res : "ERROR";
     }
+
+    // 面试题 05.03. 翻转数位
+    // 动态规划解决
+    int reverseBits(int num) {
+        int curCnt = 0, reserveCnt = 0; // 初始化当前连续1的个数和最大连续1的个数
+        int maxLen = 0; // 初始化最大连续1的个数
+
+        for (int i = 0; i < 32; i++) { // 遍历32位二进制数
+            if (num & 1 == 1) { // 如果当前位为1
+                reserveCnt++; // 增加连续1的个数
+                curCnt++; // 增加当前连续1的个数
+            } else { // 如果当前位为0
+                reserveCnt = curCnt + 1; // 更新最大连续1的个数
+                curCnt = 0; // 重置当前连续1的个数
+            }
+
+            if (reserveCnt > maxLen) { // 更新最大连续1的个数
+                maxLen = reserveCnt;
+            }
+            num >>= 1; // 右移当前数，去掉最低位
+        }
+        return maxLen; // 返回最大连续1的个数
+    }
 };
